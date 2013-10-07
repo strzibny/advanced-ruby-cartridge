3fc62a01d28f64bb7f7f7c65749eb0e137af11fc
pbrazdil <p.brazdil@redhat.com>
2013-10-07 10:17:46 +0200

    puma config changed for best performance

--- servers/puma/config.rb
+++ servers/puma/config.rb
@@ -3,5 +3,6 @@ daemonize
 pidfile "#{ENV['OPENSHIFT_SERVER_PID']}"
 state_path "#{ENV['OPENSHIFT_RUBY_DIR']}run/server.state"
 stdout_redirect "#{ENV['OPENSHIFT_SERVER_STDOUT_LOG']}", "#{ENV['OPENSHIFT_SERVER_STDERR_LOG']}"
-threads 0, 16
+threads 0, 4
+#workers 2
 bind "tcp://#{ENV['OPENSHIFT_RUBY_IP']}:#{ENV['OPENSHIFT_RUBY_PORT']}"
