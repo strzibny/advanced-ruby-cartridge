ffc123bc480cd198064be025ccc6522755ecd094
pbrazdil <p.brazdil@gmail.com>
2013-11-03 16:37:28 +0100

    separeted logs for each server

--- servers/puma/config.rb
+++ servers/puma/config.rb
@@ -2,7 +2,7 @@ environment 'production'
 daemonize
 pidfile "#{ENV['OPENSHIFT_RUBY_SERVER_PID']}"
 state_path "#{ENV['OPENSHIFT_RUBY_DIR']}run/server.state"
-stdout_redirect "#{ENV['OPENSHIFT_RUBY_SERVER_STDOUT_LOG']}", "#{ENV['OPENSHIFT_RUBY_SERVER_STDERR_LOG']}"
+stdout_redirect "#{ENV['OPENSHIFT_RUBY_LOG_DIR']}/puma_stdout.log", "#{ENV['OPENSHIFT_RUBY_LOG_DIR']}/puma_stderr.log"
 threads 0, 4
 #workers 2
 bind "tcp://#{ENV['OPENSHIFT_RUBY_IP']}:#{ENV['OPENSHIFT_RUBY_PORT']}"
